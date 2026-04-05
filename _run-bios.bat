fc079995b1b3226340b06f1cae079eb69fe78b9f
LMN128 <kucera@8bc.com>
2026-04-05 18:22:20 +0200

    Remane files

--- _run-bios.bat
+++
@@ -1,3 +0,0 @@
-sjasmplus bios.a80 --lst=bit.lst --lstlab   
-start c:\LnxSpectrum\LnxSpectrum.exe setup.sna
-timeout 10
---
+++ _run-setup.bat
@@ -0,0 +1,3 @@
+sjasmplus setup.a80 --lst=bit.lst --lstlab   
+start c:\LnxSpectrum\LnxSpectrum.exe setup.sna
+timeout 10
